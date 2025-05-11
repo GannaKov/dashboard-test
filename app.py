1515ca89b4abe35b3763b4a45b066d6196fc0146
GannaKov <zlatta2000@gmail.com>
2025-05-12 00:49:43 +0200

    save

--- app.py
+++ app.py
@@ -142,5 +142,6 @@ fig_hourly_sales = px.bar(
 
 #st.plotly_chart(fig_hourly_sales)
 left_column, right_column = st.columns(2)
+
 left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
 right_column.plotly_chart(fig_product_sales, use_container_width=True)
