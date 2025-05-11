a92570e2ca74f6375ec7cec93b1b7b806b415979
GannaKov <zlatta2000@gmail.com>
2025-05-11 21:12:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# pip freeze > requirements.txt to generate a requirements.txt file with the current environment's packages
+
+# pip install -r requirements.txt to install the packages listed in requirements.txt
+
+Local URL: http://localhost:8501
+
+in Python terminal
+streamlit app.py
---
+++ app.py
@@ -0,0 +1,94 @@
+import pandas as pd # pip install pandas openpyxl
+import streamlit as st  # pip install streamlit
+import plotly.express as px  # pip install plotly-express
+
+
+
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+st.title("Sales Dashboard")
+st.write("Welcome to the sales dashboard!")
+# ---- READ EXCEL ----
+@st.cache_data
+def get_data_from_excel():
+    df = pd.read_excel(
+        io="supermarkt_sales.xlsx",
+        engine="openpyxl",
+        sheet_name="Sales",
+        skiprows=3,
+        usecols="B:R",
+        nrows=1000,
+    )
+    # Add 'hour' column to dataframe
+    #df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+    return df
+    print(df.head())
+
+df = get_data_from_excel()
+
+
+# ---- SIDEBAR ----
+LOGO_URL_LARGE="images/logo200.png"
+LOGO_URL_SMALL="images/logo200.png"
+
+st.logo(
+    LOGO_URL_LARGE,
+    size="large",
+    link="https://streamlit.io/gallery",
+    icon_image=LOGO_URL_SMALL,
+)
+
+st.sidebar.header("Please Filter Here:")
+city = st.sidebar.multiselect(
+    "Select the City:",
+    options=df["City"].unique(),
+    default=df["City"].unique()
+)
+
+customer_type = st.sidebar.multiselect(
+    "Select the Customer Type:",
+    options=df["Customer_type"].unique(),
+    default=df["Customer_type"].unique(),
+)
+
+gender = st.sidebar.multiselect(
+    "Select the Gender:",
+    options=df["Gender"].unique(),
+    default=df["Gender"].unique()
+)
+
+df_selection = df.query(
+    "City == @city & Customer_type ==@customer_type & Gender == @gender"
+)
+
+st.dataframe(df_selection)
+
+
+# Check if the dataframe is empty:
+if df_selection.empty:
+    st.warning("No data available based on the current filter settings!")
+    st.stop() # This will halt the app from further execution.
+
+
+# ---- MAINPAGE ----
+st.title(":bar_chart: Sales Dashboard")
+st.markdown("##")
+
+# TOP KPI's
+total_sales = int(df_selection["Total"].sum())
+average_rating = round(df_selection["Rating"].mean(), 1)
+star_rating = ":star:" * int(round(average_rating, 0))
+average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    st.subheader("Total Sales:")
+    st.subheader(f"US $ {total_sales:,}")
+with middle_column:
+    st.subheader("Average Rating:")
+    st.subheader(f"{average_rating} {star_rating}")
+with right_column:
+    st.subheader("Average Sales Per Transaction:")
+    st.subheader(f"US $ {average_sale_by_transaction}")
+
+st.markdown("""---""")
