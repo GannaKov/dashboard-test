1bb431e83e2eec428f7aa497112964f52e87dd47
GannaKov <zlatta2000@gmail.com>
2025-05-13 22:42:56 +0200

    added Distribution by City

--- page2.py
+++ page2.py
@@ -111,8 +111,8 @@ third_column.plotly_chart(fig_customer_type, use_container_width=True)
 #fourth
 st.plotly_chart(fig_city)
 st.markdown("""---""") 
-#-----------------------
-#-----------------------
+#=======================================
+#=======================================
 
 # Distribution in Age Group
 # By City
@@ -146,14 +146,12 @@ fig_age_by_customer_type = px.pie(
     age_group_counts_by_customer_type,
     names='Age Group',
     values='Count',
-    title=f"Distribution by Age Group in {customer_type}",
+    title=f'Distribution by Age Group in type "{customer_type}"',
     color_discrete_sequence=px.colors.qualitative.Bold
 )
 
 #--------Gender-------
-# df_selection_for_age = df.query(
-#     "City == @city & Customer_type ==@customer_type & Gender == @gender " 
-# )
+
 df_selection_for_age_by_gender = df.query(
     "Gender == @gender" 
 )
@@ -165,7 +163,7 @@ fig_age_by_gender = px.pie(
     age_group_counts_by_gender,
     names='Age Group',
     values='Count',
-    title=f"Distribution by Age Group in {gender}",
+    title=f"Distribution by Age Group for {gender}",
     color_discrete_sequence=px.colors.qualitative.Bold
 )
 
@@ -174,3 +172,95 @@ first_age_column, second_age_column,third_age_column , = st.columns(3,gap="small
 first_age_column.plotly_chart(fig_age_by_city, use_container_width=True)
 second_age_column.plotly_chart(fig_age_by_customer_type, use_container_width=True)
 third_age_column.plotly_chart(fig_age_by_gender, use_container_width=True)
+#=========================================
+# df_selection_for_age = df.query(
+#     "City == @city & Customer_type ==@customer_type & Gender == @gender " 
+# )
+# Distribution in City
+# By Age Group
+
+st.subheader(f"Distribution by City")
+df_selection_for_city_by_age = df.query(
+    "`Age Group` == @age_group" 
+)
+
+city_group_counts_by_age = df_selection_for_city_by_age['City'].value_counts().sort_index().reset_index()
+
+city_group_counts_by_age.columns = ['City', 'Count'] #alreade done in the above code
+fig_city_by_age = px.pie(
+    city_group_counts_by_age,
+    names='City',
+    values='Count',
+    title=f"Distribution by City in {age_group}",
+    color_discrete_sequence=px.colors.qualitative.Plotly
+)
+#st.plotly_chart(fig_city_by_age)
+#------------City by customer Type---
+
+df_selection_for_city_by_customer_type = df.query(
+    "Customer_type ==@customer_type" 
+)
+
+city_group_counts_by_customer_type = df_selection_for_city_by_customer_type['City'].value_counts().sort_index().reset_index()
+
+city_group_counts_by_customer_type.columns = ['City', 'Count']
+fig_city_by_customer_type = px.pie(
+    city_group_counts_by_customer_type,
+    names='City',
+    values='Count',
+    title=f'Distribution by City in type "{customer_type}"',
+    color_discrete_sequence=px.colors.qualitative.Plotly
+)
+#st.plotly_chart(fig_city_by_customer_type)
+#--------City by Gender-------
+
+df_selection_for_city_by_gender = df.query(
+    "Gender == @gender" 
+)
+
+city_group_counts_by_gender = df_selection_for_city_by_gender['City'].value_counts().sort_index().reset_index()
+
+city_group_counts_by_gender.columns = ['City', 'Count'] 
+fig_city_by_gender = px.pie(
+    city_group_counts_by_gender,
+    names='City',
+    values='Count',
+    title=f"Distribution by City for {gender}",
+    color_discrete_sequence=px.colors.qualitative.Plotly
+)
+#st.plotly_chart(fig_city_by_gender)
+
+first_city_column, second_city_column,third_city_column  = st.columns(3,gap="small")
+first_city_column.plotly_chart(fig_city_by_age, use_container_width=True)
+second_city_column.plotly_chart(fig_city_by_customer_type, use_container_width=True)
+third_city_column.plotly_chart(fig_city_by_gender, use_container_width=True)
+
+#=========================================
+# df_selection_for_age = df.query(
+#     "City == @city & Customer_type ==@customer_type & Gender == @gender " 
+# )
+# Distribution in Gender
+st.subheader(f"Distribution by Gender")
+# By City
+
+
+# By Customer Type
+
+
+
+# By Age Group
+
+#=========================================
+# df_selection_for_age = df.query(
+#     "City == @city & Customer_type ==@customer_type & Gender == @gender " 
+# )
+# Distribution by Customer Type
+st.subheader(f"Distribution by Customer Type")
+# By City
+
+
+# By Age Group
+
+
+
+# By Gender
