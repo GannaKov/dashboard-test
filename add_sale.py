37383add06bf4c86d46571be23545990393b083b
GannaKov <zlatta2000@gmail.com>
2025-05-16 00:48:52 +0200

    clean

--- add_sale.py
+++ add_sale.py
@@ -1,7 +1,7 @@
-import pandas as pd # pip install pandas openpyxl
+#import pandas as pd # pip install pandas openpyxl
 import streamlit as st  # pip install streamlit
-import plotly.express as px  # pip install plotly-express
-from data import get_data_from_excel
+#import plotly.express as px  # pip install plotly-express
+#from data import get_data_from_excel
 st.html("./custom.css")
 
 st.title("📋 Sales Data")
