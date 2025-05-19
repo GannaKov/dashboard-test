5448de6b0d01f4c6d0177caf0468d904f02c4d6a
GannaKov <zlatta2000@gmail.com>
2025-05-19 20:07:12 +0200

    added some imputs

--- add_sale.py
+++ add_sale.py
@@ -11,13 +11,13 @@ st.write("The current movie title is", invoice_id)
 branch = st.segmented_control(
     "Branch", ["A","B","C"], selection_mode="single", default ="A",  help="Select a branch"
 )
-st.markdown(f"Your selected options: {branch}.")
+
 city= st.selectbox(
     "City",
     ["Berlin", "Paris", "Rome", "Madrid", "Amsterdam", "Vienna", "Prague", "Lisbon"],
     help="Select a city",
 )
-st.write("The current movie title is", city)
+
 customer_type =st.radio(
     "Customer Type",
     ["Member", "Normal"],
@@ -31,14 +31,36 @@ product_line = st.selectbox(
 )
 
 #--------------
-age=number = st.number_input(
+age = st.number_input(
     "Age", value=None, placeholder="18", min_value=15, max_value=120, step=1, help="Enter age"
 )
 
 
-rating=number = st.number_input(
+rating = st.number_input(
     "Rating", value=None, placeholder="10", min_value=0.0, max_value=10.0, step=0.1, help="Enter rating"
 )
 # if selected is not None:
 #     st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
+quantity = st.number_input(
+    "Quantity", value=None, placeholder="5", min_value=1, max_value=40, step=1, help="Enter quantity"
+)
 
+price = st.number_input(
+    "Unit price", value=None, placeholder="33.2", min_value=0.0, max_value=1000.0, step=0.1, help="Enter price"
+)
+# print(type(price))
+# print(type(quantity))
+if price and quantity:
+    tax = quantity * price * 0.05
+    total = quantity * price + tax
+    cogs = quantity * price
+    margin_percent = 4.7619
+    gross_income = total - cogs
+    
+st.write("Tax 5%: ", round(tax, 4) if price and quantity else "-")
+st.write("Total: ", round(total , 4) if price and quantity else "-")
+st.write("Cogs: ", round(cogs, 4) if price and quantity else "-")
+st.write("Margin percent: ", round(margin_percent, 4) if price and quantity else "-")
+st.write("Gross income: ", round(gross_income, 4) if price and quantity else "-")
+
+#----------------
