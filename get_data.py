f007adc04693b5bd71cb4130a60433e77f08ab85
GannaKov <zlatta2000@gmail.com>
2025-05-24 18:20:49 +0200

    added sort "Date"

--- get_data.py
+++ get_data.py
@@ -14,6 +14,7 @@ def get_data_from_excel():
         #nrows=1000,
     )
     df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d").dt.date
+    df["Time"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.time
     df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
     #df["month"] = pd.to_datetime(df["Date"], format="%Y-%m-%d").dt.month
     #df["year"] = pd.to_datetime(df["Date"], format="%Y-%m-%d").dt.year
--- view_data.py
+++ view_data.py
@@ -9,4 +9,14 @@ from get_data import get_data_from_excel
 df = get_data_from_excel()
 st.title("📋 Sales Data")
 st.markdown("")
-st.dataframe(df, use_container_width=True)
+st.dataframe(df.sort_values(by=['Date']), use_container_width=True)
+# ---- EDIT DATA ----
+# Uncomment the following lines to enable data editing and saving functionality
+""" edited_df = st.data_editor(df)
+if st.button("💾 Save Changes"):
+    try:
+        # Save the edited data back to the Excel file
+        edited_df.to_excel("sales.xlsx", index=False, startrow=3)  # assuming headers start from row 4
+        st.success("Changes have been successfully saved to the file!")
+    except Exception as e:
+        st.error(f"An error occurred while saving: {e}") """
